4569f427bba14d55f759f26779365f67fcbf19cd
raihanulR <raihanul409@gmail.com>
2026-08-30 07:38:19 +0600

    fix: import Link component from react-router-dom in MyBookings page

--- src/pages/MyBookings.jsx
+++ src/pages/MyBookings.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import api from '../utils/api';
 import Loader from '../components/Loader';
 import useDocumentTitle from '../hooks/useDocumentTitle';
